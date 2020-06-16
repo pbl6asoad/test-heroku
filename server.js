8c3e1f59786b058728f9db0a868fe7e94d242447
Ilya <pbl6a.soad@gmail.com>
2020-06-16 12:38:57 +0300

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.use(express.static(__dirname + "/src/index"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(__dirname + "/src/index.html");
+});
+app.listen(port, () => console.log(`Example app listening on port port!`));
